07b0ed58a88fe22d2a30a858f8e446a788e16b1e
arthur-siqueira <arthuths13@gmail.com>
2023-09-09 00:41:38 -0300

    Adicionando exercicios sobre controle de fluxo

--- controle-fluxo/src/LendoNomes.java
+++ controle-fluxo/src/LendoNomes.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class LendoNomes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] nomes = new String[10];
+
+        for(int i = 1; i<10; i++) {
+            nomes[i] = sc.nextLine();
+        }
+
+        System.out.println(nomes[3]);
+        System.out.println(nomes[7]);
+        System.out.println(nomes[9]);
+    }
 }
