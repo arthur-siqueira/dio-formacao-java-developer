5cd57f8b4a4155b31f107a15822111e503e5136c
arthur-siqueira <arthuths13@gmail.com>
2023-10-03 06:32:37 -0300

    Adicionando conteúdo completo sobre Filas

---
+++ fila/src/main/java/No.java
@@ -0,0 +1,36 @@
+public class No {
+
+    private Object object;
+    private No refNo;
+
+    public No() {
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public No(Object object) {
+        this.refNo = null;
+        this.object = object;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "object=" + object +
+                '}';
+    }
+}
