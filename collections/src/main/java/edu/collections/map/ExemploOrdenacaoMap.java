bbbfa1fbe55fea7345cfc128d2f1af3a4af84e46
arthur-siqueira <arthuths13@gmail.com>
2023-10-19 10:57:56 -0300

    Adicionando o exemplo sobre ordenação Map

---
+++ collections/src/main/java/edu/collections/map/ExemploOrdenacaoMap.java
@@ -0,0 +1,111 @@
+package edu.collections.map;
+
+import java.util.*;
+
+/*
+Dado as informações sobre os livros a seguir, coloque-os em uma collection Map
+e realize as operações solicitadas
+
+Autor = Hawking, Stephen - Livro = nome: Uma Breve História do Tempo. páginas: 256
+Autor = Duhig, Charles - Livro = nome: O Poder do Hábito. páginas: 408
+Autor = Harari, Yuval Noah - Livro = nome: 21 Lições para o Século 21. páginas: 432
+*/
+
+
+public class ExemploOrdenacaoMap {
+    public static void main(String[] args) {
+
+        System.out.println("Ordem aleatória");
+        Map<String, Livro> meusLivros = new HashMap<>() {{
+            put(" Hawking, Stephen", new Livro("Uma Breve História do Tempo", 256));
+            put(" Duhigg, Charles", new Livro("O Poder do Hábito", 408));
+            put(" Harari, Yuval Noah", new Livro("21 Lições Para o Século 21", 432));
+
+        }};
+        for (Map.Entry<String, Livro> livro : meusLivros.entrySet())
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+        System.out.println("Ordem Inserção");
+        Map<String, Livro> meusLivros1 = new LinkedHashMap<>() {{
+            put(" Hawking, Stephen", new Livro("Uma Breve História do Tempo", 256));
+            put(" Duhigg, Charles", new Livro("O Poder do Hábito", 408));
+            put(" Harari, Yuval Noah", new Livro("21 Lições Para o Século 21", 432));
+        }};
+        for (Map.Entry<String, Livro> livro : meusLivros1.entrySet())
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+        System.out.println("Ordem alfabética autores");
+        Map<String, Livro> meusLivros2 = new TreeMap<>(meusLivros1);
+        for (Map.Entry<String, Livro> livro : meusLivros2.entrySet())
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+        System.out.println("Ordem alfabética nomes dos livros");
+        Set<Map.Entry<String, Livro>> meusLivros3 = new TreeSet<>(new ComparatorNome());
+        meusLivros3.addAll(meusLivros.entrySet());
+        for (Map.Entry<String, Livro> livro : meusLivros3)
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+        System.out.println("Ordem por número de páginas");
+        Set<Map.Entry<String, Livro>> meusLivros4 = new TreeSet<>(new ComparatorNumPaginas());
+        meusLivros4.addAll(meusLivros.entrySet());
+        for (Map.Entry<String, Livro> livro : meusLivros4)
+            System.out.println(livro.getKey() + " - " + livro.getValue().getPaginas());
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer paginas;
+
+    public Livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return nome.equals(livro.nome) && paginas.equals(livro.paginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, paginas);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", paginas=" + paginas +
+                '}';
+    }
+}
+
+class ComparatorNome implements Comparator<Map.Entry<String, Livro>>{
+
+    @Override
+    public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
+        return l1.getValue().getNome().compareToIgnoreCase(l2.getValue().getNome());
+    }
+}
+
+class ComparatorNumPaginas implements Comparator<Map.Entry<String, Livro>> {
+
+    @Override
+    public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
+        return l1.getValue().getPaginas().compareTo(l2.getValue().getPaginas());
+    }
+}
+
+
