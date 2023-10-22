2f64b00f88618859f9ae8adf437dab8ae281313b
arthur-siqueira <arthuths13@gmail.com>
2023-10-21 21:29:48 -0300

    Adicionando exercício proposto 01 sobre a interface Map

--- collections/src/main/java/edu/collections/map/ExercicioProposto01.java
+++ collections/src/main/java/edu/collections/map/ExercicioProposto01.java
@@ -1,7 +1,6 @@
 package edu.collections.map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ExercicioProposto01 {
 
@@ -23,7 +22,54 @@ public class ExercicioProposto01 {
         }
 
         System.out.println("A população de PE é: " + estadosNordeste.get("PE"));
+
+        System.out.println("Estados e suas populações na ordem informada:");
+        for (Map.Entry<String, Integer> entry : estadosNordeste.entrySet()) {
+            System.out.println("Estado: " + entry.getKey() + ", População: " + entry.getValue());
+        }
+
+
+        System.out.println("\nEstados e suas populações em ordem alfabética:");
+        TreeMap<String, Integer> populacaoEstadosOrdenado = new TreeMap<>(estadosNordeste);
+        for (Map.Entry<String, Integer> entry : populacaoEstadosOrdenado.entrySet()) {
+            System.out.println("Estado: " + entry.getKey() + ", População: " + entry.getValue());
+        }
+
+        int menorPopulacao = Collections.min(estadosNordeste.values());
+        String estadoMenorPopulacao = "";
+        for (Map.Entry<String, Integer> entry : estadosNordeste.entrySet()) {
+            if (entry.getValue() == menorPopulacao) {
+                estadoMenorPopulacao = entry.getKey();
+                break;
+            }
+        }
+        System.out.println("\nEstado com a menor população: " + estadoMenorPopulacao + ", População: " + menorPopulacao);
+
+        int maiorPopulacao = Collections.max(estadosNordeste.values());
+        String estadoMaiorPopulacao = "";
+        for (Map.Entry<String, Integer> entry : estadosNordeste.entrySet()) {
+            if (entry.getValue() == maiorPopulacao) {
+                estadoMaiorPopulacao = entry.getKey();
+                break;
+            }
+        }
+        System.out.println("Estado com a maior população: " + estadoMaiorPopulacao + ", População: " + maiorPopulacao);
+
+        int somaPopulacao = estadosNordeste.values().stream().mapToInt(Integer::intValue).sum();
+        System.out.println("\nSoma da população dos estados: " + somaPopulacao);
+
+        double mediaPopulacao = estadosNordeste.values().stream().mapToInt(Integer::intValue).average().orElse(0);
+        System.out.println("Média da população dos estados: " + mediaPopulacao);
+
+        estadosNordeste.values().removeIf(populacao -> populacao < 4000000);
+        System.out.println("\nEstados com população maior que 4.000.000:");
+        for (Map.Entry<String, Integer> entry : estadosNordeste.entrySet()) {
+            System.out.println("Estado: " + entry.getKey() + ", População: " + entry.getValue());
+        }
+
+        estadosNordeste.clear();
+        System.out.println("\nDicionário de estados foi apagado.");
+
+        System.out.println("O dicionário está vazio: " + estadosNordeste.isEmpty());
     }
-
-
 }
---
+++ collections/src/main/java/edu/collections/map/ExercicioProposto02.java
@@ -0,0 +1,9 @@
+package edu.collections.map;
+
+public class ExercicioProposto02 {
+
+
+
+}
+
+
