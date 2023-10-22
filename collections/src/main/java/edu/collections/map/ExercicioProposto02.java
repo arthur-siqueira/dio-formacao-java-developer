d3ce69da51006e416efb0960506fa6f542a1c4e5
arthur-siqueira <arthuths13@gmail.com>
2023-10-21 21:40:23 -0300

    Adicionando exercício proposto 02 sobre a interface Map

--- collections/src/main/java/edu/collections/map/ExercicioProposto02.java
+++ collections/src/main/java/edu/collections/map/ExercicioProposto02.java
@@ -1,8 +1,32 @@
 package edu.collections.map;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class ExercicioProposto02 {
 
+    public static void main(String[] args) {
 
+        Map<Integer, Integer> resultados = new HashMap<>();
+
+        for (int i = 0; i < 100; i++) {
+            int resultadoLancamento = simularLancamentoDado();
+            resultados.put(resultadoLancamento, resultados.getOrDefault(resultadoLancamento, 0) + 1);
+        }
+
+        System.out.println("Resultados dos lançamentos do dado:");
+        for (Map.Entry<Integer, Integer> entry : resultados.entrySet()) {
+            System.out.println("Número: " + entry.getKey() + ", Frequência: " + entry.getValue());
+        }
+
+    }
+
+    // Método para simular o lançamento de um dado de seis lados
+    private static int simularLancamentoDado() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
 
 }
 
