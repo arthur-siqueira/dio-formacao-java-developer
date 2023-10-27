29c1fe71334116854ce00a41020a5e4898d2778e
arthur-siqueira <arthuths13@gmail.com>
2023-10-27 00:11:37 -0300

    Interligando as classes de domínio e finalizando aplicação

--- desafio-dio-POO/src/main/java/Main.java
+++ desafio-dio-POO/src/main/java/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,41 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        System.out.println("Informações dev Arthur:\n");
+
+        Dev devArthur = new Dev();
+        devArthur.setNome("Arthur");
+        devArthur.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Arthur: " + devArthur.getConteudosInscritos());
+        devArthur.progredir();
+        devArthur.progredir();
+        devArthur.progredir();
+        System.out.println("Conteúdos Inscritos Arthur: " + devArthur.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Arthur: " + devArthur.getConteudosConcluidos());
+        System.out.println("XP: " + devArthur.calcularTotalXp());
+
+        System.out.println("-------------------------");
+
+        System.out.println("Informações dev Filipe:\n");
+
+        Dev devFilipe = new Dev();
+        devFilipe.setNome("Filipe");
+        devFilipe.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Filipe: " + devFilipe.getConteudosInscritos());
+        devFilipe.progredir();
+        System.out.println("Conteúdos Inscritos Filipe: " + devFilipe.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Filipe: " + devFilipe.getConteudosConcluidos());
+        System.out.println("XP: " + devFilipe.calcularTotalXp());
     }
 }
